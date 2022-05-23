cb06426108bd19a101a385393d18235ba2ff7b2c
Burak <burakinan1@hotmail.com>
2022-05-23 23:08:49 +0200

    Fix income base rate setting name

--- src/main/kotlin/com/github/buracc/healthybot/discord/SettingConstants.kt
+++ src/main/kotlin/com/github/buracc/healthybot/discord/SettingConstants.kt
@@ -8,7 +8,7 @@ object SettingConstants {
     const val CRIME_SETTING = "crime_enabled"
     const val INCOME_MIN_RATIO = "income_min_ratio"
     const val INCOME_MAX_RATIO = "income_max_ratio"
-    const val INCOME_BASE_RATE = "income_base_rate"
+    const val INCOME_BASE_RATE = "income_base_rate_max"
     const val WORK_CD = "work_cooldown_ms"
     const val ROB_CD = "rob_cooldown_ms"
     const val CRIME_CD = "crime_cooldown_ms"
