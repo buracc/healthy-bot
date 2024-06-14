f855fb7fd3266804f21bfecf4030c81b126bb17d
Burak <burakinan1@hotmail.com>
2024-06-14 19:28:59 +0200

    map reminder to machine's timezone first

--- src/main/kotlin/com/github/buracc/healthybot/discord/scheduled/ReminderUpdater.kt
+++ src/main/kotlin/com/github/buracc/healthybot/discord/scheduled/ReminderUpdater.kt
@@ -4,8 +4,6 @@ import com.github.buracc.healthybot.service.ReminderService
 import net.dv8tion.jda.api.entities.Guild
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.time.Duration
-import java.time.Instant
 import java.time.ZonedDateTime
 
 @Component
@@ -24,26 +22,21 @@ class ReminderUpdater(
     @Scheduled(cron = "0 */10 * * * *")
     fun checkAndAnnounce() {
         val remindersToday = reminderService.getAll()
+            .map {
+                it.copy(
+                    remindDateString = it.remindDate.withZoneSameInstant(ZonedDateTime.now().zone)
+                        .format(ReminderService.format)
+                )
+            }
             .filter { it.remindDate.toLocalDate() == ZonedDateTime.now().toLocalDate() }
             .sortedBy { it.remindDate }
             .take(3)
 
-        // check channels in category and delete ones that are not in remindersToday
         val category = guild.getCategoryById(category) ?: return
 
         category.voiceChannels.forEach { it.delete().queue() }
 
         for (reminder in remindersToday) {
-            val now = Instant.now()
-            val duration = Duration.between(now, reminder.remindDate.toInstant())
-            val sb = StringBuilder()
-            val days = duration.toDaysPart()
-            val hours = duration.toHoursPart()
-            val minutes = duration.toMinutesPart()
-            if (days > 0) sb.append("$days days ")
-            if (hours > 0) sb.append("$hours hours ")
-            if (minutes > 0) sb.append("$minutes minutes")
-
             val eventTime = reminder.remindDate.toLocalTime().toString()
 
             guild.createVoiceChannel("$eventTime - ${reminder.message}")
